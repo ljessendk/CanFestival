21bd3536d9a3400b61465140b0eafa0e593d0507
lbessard <devnull@localhost>
2006-08-10 17:13:23 +0200

    Bug on NoteBook in Windows corrected

--- objdictgen/objdictedit.py
+++ objdictgen/objdictedit.py
@@ -981,8 +981,9 @@ class objdictedit(wx.Frame):
         event.Skip()
 
     def OnFileSelectedChanged(self, event):
-        selected = self.FileOpened.GetSelection()
+        selected = event.GetSelection()
         # At init selected = -1
+        print selected
         if selected >= 0:
         	self.Manager.ChangeCurrentNode(selected)
         	self.RefreshBufferState()
@@ -1701,9 +1702,9 @@ class MapVariableDialog(wx.Dialog):
     def GetValues(self):
         if self.radioButton1.GetValue():
             struct = 1
-        elif self.radioButton3.GetValue():
-            struct = 3
         elif self.radioButton2.GetValue():
+            struct = 3
+        elif self.radioButton3.GetValue():
             struct = 7
         name = self.IndexName.GetValue()
         index = eval(self.Index.GetValue())
