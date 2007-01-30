143078404748e08fdf30f3af195bdf8a34b37fde
frdupin <devnull@localhost>
2007-01-30 10:28:22 +0100

    forgot some comments ...

--- src/sdo.c
+++ src/sdo.c
@@ -20,8 +20,8 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#define DEBUG_WAR_CONSOLE_ON 
-#define DEBUG_ERR_CONSOLE_ON 
+/* #define DEBUG_WAR_CONSOLE_ON */
+/* #define DEBUG_ERR_CONSOLE_ON */
 
 #include "objacces.h"
 #include "sdo.h"
