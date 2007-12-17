e93ae1c0940280f2b817987dfb6001315e8cdc34
etisserant <devnull@localhost>
2007-12-17 18:14:42 +0100

    Fixed horrible typo in states.c thanks to Martin Hollenweger.

--- src/states.c
+++ src/states.c
@@ -267,12 +267,12 @@ void setNodeId(CO_Data* d, UNS8 nodeId)
 
   if(offset){
     /* Adjust COB-ID Client->Server (rx) only id already set to default value*/
-    if(*(UNS32*)d->objdict[offset].pSubindex[1].pObject = 0x600 + *d->bDeviceNodeId){
+    if(*(UNS32*)d->objdict[offset].pSubindex[1].pObject == 0x600 + *d->bDeviceNodeId){
       /* cob_id_client = 0x600 + nodeId; */
       *(UNS32*)d->objdict[offset].pSubindex[1].pObject = 0x600 + nodeId;
     }
     /* Adjust COB-ID Server -> Client (tx) only id already set to default value*/
-    if(*(UNS32*)d->objdict[offset].pSubindex[2].pObject = 0x580 + *d->bDeviceNodeId){
+    if(*(UNS32*)d->objdict[offset].pSubindex[2].pObject == 0x580 + *d->bDeviceNodeId){
       /* cob_id_server = 0x580 + nodeId; */
       *(UNS32*)d->objdict[offset].pSubindex[2].pObject = 0x580 + nodeId;
     }
