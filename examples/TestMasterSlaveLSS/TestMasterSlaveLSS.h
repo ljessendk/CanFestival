1a3a7361f90c4c04a766f3584da71fea198f56c5
etisserant <devnull@localhost>
2008-02-19 14:54:06 +0100

    Removed old commented code.

--- examples/TestMasterSlaveLSS/TestMasterSlaveLSS.h
+++ examples/TestMasterSlaveLSS/TestMasterSlaveLSS.h
@@ -26,8 +26,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #define eprintf(...) printf (__VA_ARGS__)
 #endif
 
-/*UNS8 canSend(CAN_HANDLE fd0, Message *m);*/
-
 #include "canfestival.h"
 
 /*
