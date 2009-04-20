a405cc27955acfd6edf4b05b5a3c72d39b39c9b0
lbessard <devnull@localhost>
2009-04-20 17:42:36 +0200

    Bug avoided reset PDO mapping fixed

--- objdictgen/eds_utils.py
+++ objdictgen/eds_utils.py
@@ -513,12 +513,12 @@ def GenerateFileContent(Node, filepath):
     manufacturers = []
     
     # Remove all unused PDO
-    for entry in entries[:]:
-        if 0x1600 <= entry < 0x1800 or 0x1A00 <= entry < 0x1C00:
-            subentry_value = Node.GetEntry(entry, 1)
-            if subentry_value is None or subentry_value == 0:
-                entries.remove(entry)
-                entries.remove(entry - 0x200)
+##    for entry in entries[:]:
+##        if 0x1600 <= entry < 0x1800 or 0x1A00 <= entry < 0x1C00:
+##            subentry_value = Node.GetEntry(entry, 1)
+##            if subentry_value is None or subentry_value == 0:
+##                entries.remove(entry)
+##                entries.remove(entry - 0x200)
                 
     # For each entry, we generate the entry section or sections if there is subindexes
     for entry in entries:
