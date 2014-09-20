afd1f5d5ab63b3d1115d52488dcf51b3165a1d56
Francois Beaulier <devnull@localhost>
2014-09-20 15:13:57 +0200

    cm3/4 driver now allow bitrate setting in canInit function

--- include/cm4/canfestival.h
+++ include/cm4/canfestival.h
@@ -5,7 +5,7 @@ void initTimer(void);
 void clearTimer(void);
 
 unsigned char canSend(CAN_PORT notused, Message *m);
-unsigned char canInit(CO_Data * d, unsigned int bitrate);
+unsigned char canInit(CO_Data * d, uint32_t bitrate);
 void canClose(void);
 
 void disable_it(void);
